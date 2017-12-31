288b68de3715bbc9d436c4c4ebe9cf8b472f6d11
KirstynNatavio <kirstynatavio@hotmail.com>
2017-12-31 01:50:15 -0500

    Use local-specific currency and thousands separators

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -18,23 +18,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var splitAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    func emptyTextField() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.contentView.frame.origin.y = 500
+            self.billField.frame.size.height = 370
+            self.billField.backgroundColor = UIColor(red: 87/255, green: 226/255, blue: 226/255, alpha: 1)
+            self.billField.textColor = UIColor.white
+        })
+    }
+    
+    func contentTextField() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.contentView.frame.origin.y = 190
+            self.billField.frame.size.height = 107
+            self.billField.backgroundColor = UIColor.white
+            self.billField.textColor = UIColor(red: 87/255, green: 226/255, blue: 226/255, alpha: 1)
+        })
+    }
     
     @IBAction func moveComponents(_ sender: UITextField) {
         if sender.text == "" {
-            UIView.animate(withDuration: 0.25, animations: {
-                self.contentView.frame.origin.y = 500
-                self.billField.frame.size.height = 370
-                self.billField.backgroundColor = UIColor(red: 87/255, green: 226/255, blue: 226/255, alpha: 1)
-                self.billField.textColor = UIColor.white
-            })
+            emptyTextField()
         }
         else {
-            UIView.animate(withDuration: 0.25, animations: {
-                self.contentView.frame.origin.y = 190
-                self.billField.frame.size.height = 107
-                self.billField.backgroundColor = UIColor.white
-                self.billField.textColor = UIColor(red: 87/255, green: 226/255, blue: 226/255, alpha: 1)
-            })
+            contentTextField()
             calculateTip(self)
         }
         
@@ -55,13 +62,14 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         print("view did appear")
-        billField.becomeFirstResponder()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.title = "Tip Calculator"
+        emptyTextField()
+        billField.placeholder = (Locale.current as NSLocale).displayName(forKey: .currencySymbol, value: Locale.current.currencyCode!) ?? "$"
         stepper.minimumValue = 1.0
         stepper.isContinuous = true
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
@@ -70,6 +78,8 @@ class ViewController: UIViewController {
         self.navigationController?.view.backgroundColor = .clear
         billField.layer.borderColor = UIColor.white.cgColor
         billField.layer.borderWidth = 1.0
+        billField.becomeFirstResponder()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -82,14 +92,23 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTip(_ sender: Any) {
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = NSLocale.current
+        currencyFormatter.maximumFractionDigits = 2
+
         let tipPercentages = [0.18, 0.2, 0.25]
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tipText = tip as NSNumber
         let total = bill + tip
+        let totalText = tip as NSNumber
         let splitAmount = total / (Double(numPeopleLabel.text!) ?? 0)
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        splitAmountLabel.text = String(format: "$%.2f", splitAmount)
+        let splitAmountText = splitAmount as NSNumber
+        tipLabel.text = currencyFormatter.string(from: tipText)
+        totalLabel.text = currencyFormatter.string(from: totalText)
+        splitAmountLabel.text = currencyFormatter.string(from: splitAmountText)
         
     }
 }
